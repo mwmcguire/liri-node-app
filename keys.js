4c4c75567eefb413a1b1faa2f20b599290504df2
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-06-23 23:01:27 -0400

    revised movie-this and spotify-this-song functions

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+console.log('keys is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const Spotify = require('node-spotify-api');
 
 // Import API keys from keys.js file
-var keys = require('./keys.js');
-var spotify = new Spotify(keys.spotify);
+const keys = require('./keys.js');
+const spotify = new Spotify(keys.spotify);
+
 
 
 // COMMANDS:
@@ -11,9 +13,16 @@ var spotify = new Spotify(keys.spotify);
 // movie-this
 // do-what-it-says
 
-var command = process.argv[2];
 
 
+const command = process.argv[2];
+let query = '';
+for (let i = 3; i < process.argv.length; i++) {
+  query = query + " " + process.argv[i];
+};
+
+
+// --------------------------------------------------------------
 // concert-this
 // var queryURL = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
 
@@ -21,47 +30,103 @@ var command = process.argv[2];
 
 // spotify-this-song : Search Spotify API by song name
 // display: artist, song name, preview link, album
-var spotifyThisSong = (query) => {
-  var spotify = require('node-spotify-api');
-  var query = process.argv[3];
-
-  spotify.search({ type: 'track', query: query}, function(err, data) {
+const spotifyThisSong = () => {
+  spotify.search({ type: 'track', query: "love shack"}, (err, data) => {
     if (err) {
       return console.log ('Error occurred: ' + err);
     }
+  
+    const artists = [];
+    for (let i = 0; i < data.tracks.items.length; i++) {
+      artists.push(data.tracks.items[i].artists);
+    }
 
-    console.log(JSON.stringify(data));
+    console.log(artists);
+    if (artists){
+      artists.forEach(function (element) {
+        console.log(`Artists: ${element[0].name}`);
+      });
+    }
+
+    const songs = (data.tracks.items[0]);
+    console.log("album name: " + songs.album.name);
+    //console.log("artist(s): " + songs[i].artists.names);
+    console.log("song name: " + songs.name);
+    console.log("preview song: " + songs.external_urls.spotify);
+
+    console.log("----------------------------------------");
   });
 }
 
 
+// --------------------------------------------------------------
 // movie-this : Search OMDB API by movie name
 // display: title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
-var movieThis = (query) => {
-  var query = process.argv[3];
-  var request = require('request');
-  var omdbURL = `http://www.omdbapi.com/?t=${query}&y=&plot=short&apikey=trilogy`;
-  
-  request(omdbURL, function(err, data) {
-    console.log(`
-      Title: ${data.title}\n
-      Release Year: ${data.year}\n
-      IMDB Rating: ${data.imdbRating}\n
-      Rotten Tomatoes Rating: ${data.ratings[1].value}\n
-      Country: ${data.country}\n
-      Language: ${data.language}\n
-      Plot: ${data.plot}\n
-      Actors: ${data.actors}`);
+const movieThis = () => {
+  const request = require("request");
+  const omdbURL = `http://www.omdbapi.com/?t=${query}&y=&plot=short&apikey=trilogy`;
+
+  console.log(`
+              =========================
+                  CALLING OMDB API...  
+              =========================
+              `)
+
+  request(omdbURL, function(err, data, body) {
+        if (!err && data.statusCode === 200) {
+            const info = JSON.parse(body);
+            console.log(`
+            Title: ${info.Title}\n
+            Release Year: ${info.YeTiar}\n
+            IMDB Rating: ${info.imdbRating}\n
+            
+            Country: ${info.Country}\n
+            Language: ${info.Language}\n
+            Plot: ${info.Plot}\n
+            Actors: ${info.Actors}`);
+        }
+
+        console.log(`
+          ----------------------------------------
+        `);
+    });
+}
+
+
+// --------------------------------------------------------------
+// do-what-it-says : Using the fs Node package, LIRI will call commands from random.txt
+const doWhatItSays = () => {
+  const fs = require('fs');
+  fs.readFile('random.txt', 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    const dataArr = data.split(',');
+    console.log(dataArr[0] + " " + dataArr[1]);
+    command = dataArr[0];
+    query = dataArr[1];
+    commands();
   });
 }
 
 
-
-switch(command) {
-  case 'spotify-this-song':
-    spotifyThisSong();
-    break;
-  case 'movie-this':
-    movieThis();
-    break;
+// --------------------------------------------------------------
+const commands = () => {
+  switch(command) {
+    case 'concert-this':
+      concertThis();
+      break;
+    case 'spotify-this-song':
+      spotifyThisSong();
+      break;
+    case 'movie-this':
+      movieThis();
+      break;
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+  }
 }
+
+commands();
